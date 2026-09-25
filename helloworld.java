f89a2d0572382d89488bbf13b27d359eaad7c0a0
Gray <arseluca50@pvnccdsb.on.ca>
2026-09-25 09:15:25 -0400

    I added a scanner.

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-public class Main {
+public class helloworld {
     public static void main(String[] args) {
-
+      Scanner input = new Scanner(System.in);
       System.out.println("Hello, World!");
-
+      String dopevar = input.nextLine();
+      System.out.println(dopevar);
     }
 }
